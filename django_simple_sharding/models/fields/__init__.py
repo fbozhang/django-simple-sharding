455210be0e00a2b6dc0373ef393550986b268902
fbz <fbozhang@163.com>
2024-11-23 23:00:22 +0800

    feat: 添加雪花id模型字段

---
+++ django_simple_sharding/models/fields/__init__.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from django_simple_sharding.utils.snowflake import address_snowflake_generator
+
+
+class SnowflakeIDField(models.CharField):
+    def __init__(self, *args, snowflake_generator=None, **kwargs):
+        if snowflake_generator is None:
+            snowflake_generator = address_snowflake_generator
+        self._check_snowflake_generator(snowflake_generator)
+        self.snowflake_generator = snowflake_generator
+
+        kwargs.setdefault("max_length", 20)
+        super().__init__(*args, **kwargs)
+
+    @staticmethod
+    def _check_snowflake_generator(snowflake_generator):
+        if not hasattr(snowflake_generator, "__next__"):
+            raise ValueError(
+                "The provided snowflake_generator must be an iterable object with a __next__ method."
+            )
+
+    def pre_save(self, model_instance, add):
+        value = getattr(model_instance, self.attname)
+        if not value:
+            # generator snowflake id
+            value = str(next(self.snowflake_generator))
+            setattr(model_instance, self.attname, value)
+        return value
